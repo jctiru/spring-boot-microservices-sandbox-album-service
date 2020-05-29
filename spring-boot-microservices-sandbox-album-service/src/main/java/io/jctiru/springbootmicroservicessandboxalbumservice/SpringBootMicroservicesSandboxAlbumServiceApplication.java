9e532178c75a4062048b3ba7a660d7e051bc7fef
jctiru <jonathanchristiantiru@gmail.com>
2020-05-29 20:53:52 +0800

    Initial commit

---
+++ spring-boot-microservices-sandbox-album-service/src/main/java/io/jctiru/springbootmicroservicessandboxalbumservice/SpringBootMicroservicesSandboxAlbumServiceApplication.java
@@ -0,0 +1,13 @@
+package io.jctiru.springbootmicroservicessandboxalbumservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroservicesSandboxAlbumServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroservicesSandboxAlbumServiceApplication.class, args);
+	}
+
+}
---
+++ spring-boot-microservices-sandbox-album-service/src/test/java/io/jctiru/springbootmicroservicessandboxalbumservice/SpringBootMicroservicesSandboxAlbumServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.jctiru.springbootmicroservicessandboxalbumservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroservicesSandboxAlbumServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
